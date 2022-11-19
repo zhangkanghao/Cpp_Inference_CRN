565d5de36a22d1d11aeeac12aa8aeb190862326e
zhangkanghao <koerzhang@163.com>
2022-11-19 11:31:07 +0800

    Wav File Opration

---
+++ include/Wav_File.h
@@ -0,0 +1,90 @@
+//
+// Created by 65181 on 2022/11/1.
+//
+
+#ifndef CRN_WAV_FILE_H
+#define CRN_WAV_FILE_H
+
+#include "Eigen"
+
+#define PI 3.141592653589793f
+struct Complex {
+    float_t real, imag;
+};
+
+class Wav_File {
+public:
+    int16_t *ip_raw;
+    float_t *fp_raw;
+    float_t *data;
+    size_t wav_size;
+    Complex **spec;
+    float_t **mag;
+    float_t **phase;
+
+
+    /* 无参数构造函数 */
+    Wav_File();
+
+    /* 加载语音文件 */
+    void LoadWavFile(const char *file_path);
+
+    /* 写出语音文件 */
+    void WriteWavFile(const char *dest_path);
+
+    /* 加载语音文件 */
+    void LoadPcmFile(const char *file_path);
+
+    /* 写出语音文件 */
+    void WritePcmFile(const char *dest_path);
+
+    /* 初始化STFT */
+    void setSTFT(int16_t frame_size, int16_t frame_shift, const char *window);
+
+    /* 设置窗函数 */
+    void setWindow(const char *winType);
+
+    /* 计算STFT */
+    void STFT();
+
+    /* 计算逆STFT */
+    void ISTFT();
+
+    /* 释放内存 */
+    void FreeSource();
+
+    /* 计算幅度谱 */
+    void getMagnitude();
+
+    /* 计算相位谱 */
+    void getPhase();
+
+    /* 幅度相位计算频谱 */
+    void magToSpec();
+
+private:
+    /* wav info */
+    char id_riff[5], id_wave[5], id_fmt[5], id_data[5];
+    int32_t file_size;
+    int16_t format_tag, channels, block_align, bits_per_sample, cb_size;
+    int32_t format_length, sample_rate, avg_bytes_sec, data_size;
+
+    const char *win_type;
+    int16_t frame_size;
+    int16_t frame_shift;
+    int16_t frame_num;
+    int16_t fft_size;
+    float_t *window;
+    float_t *real;
+    float_t *imag;
+
+    void FFT(float_t param_real[], float_t param_imag[], int16_t param_n);
+
+    void bitrp(float_t param_real[], float_t param_imag[], int16_t param_n);
+
+    void IFFT(float_t param_real[], float_t param_imag[], int16_t param_n);
+
+};
+
+
+#endif //CRN_WAV_FILE_H
---
+++ main/Wav_File.cpp
@@ -0,0 +1,370 @@
+//
+// Created by 65181 on 2022/11/1.
+//
+
+#include "../include/Wav_File.h"
+#include "Eigen"
+#include <cmath>
+#include <fstream>
+#include <cstdlib>
+#include <iostream>
+
+
+Wav_File::Wav_File() {
+    this->ip_raw = nullptr;
+    this->fp_raw = nullptr;
+    this->wav_size = 0;
+}
+
+void Wav_File::LoadWavFile(const char *file_path) {
+    FILE *fp = fopen(file_path, "rb");
+    if (fp) {
+        fread(this->id_riff, sizeof(char), 4, fp);
+        this->id_riff[4] = '\0';
+        fread(&this->file_size, sizeof(int16_t), 2, fp);
+        fread(this->id_wave, sizeof(char), 4, fp);
+        this->id_wave[4] = '\0';
+        fread(this->id_fmt, sizeof(char), 4, fp);
+        this->id_fmt[4] = '\0';
+        fread(&this->format_length, sizeof(int16_t), 2, fp);
+        fread(&this->format_tag, sizeof(int16_t), 1, fp);
+        fread(&this->channels, sizeof(int16_t), 1, fp);
+        fread(&this->sample_rate, sizeof(int16_t), 2, fp);
+        fread(&this->avg_bytes_sec, sizeof(int16_t), 2, fp);
+        fread(&this->block_align, sizeof(int16_t), 1, fp);
+        fread(&this->bits_per_sample, sizeof(int16_t), 1, fp);
+        if (this->format_tag == 1) {
+            fread(this->id_data, sizeof(char), 4, fp);
+            this->id_data[4] = '\0';
+            fread(&this->data_size, sizeof(int16_t), 2, fp);
+            this->wav_size = this->data_size / sizeof(int16_t);
+            this->ip_raw = (int16_t *) malloc(this->data_size);
+            fread(this->ip_raw, sizeof(int16_t), this->wav_size, fp);
+
+            this->data = (float_t *) malloc(this->wav_size * sizeof(float_t));
+            for (int i = 0; i < this->wav_size; i++) {
+                this->data[i] = (float_t) (this->ip_raw[i] * 1.0 / 32768);
+            }
+
+        } else if (this->format_tag == 3) {
+            fread(&this->cb_size, sizeof(int16_t), 1, fp);
+            fread(this->id_data, sizeof(char), 4, fp);
+            this->id_data[4] = '\0';
+            fread(&this->data_size, sizeof(int16_t), 2, fp);
+            this->wav_size = this->data_size / sizeof(float);
+            this->fp_raw = (float *) malloc(this->data_size);
+            fread(this->fp_raw, sizeof(float), this->wav_size, fp);
+            this->data = (float_t *) malloc(this->wav_size * sizeof(float_t));
+            for (int i = 0; i < this->wav_size; i++) {
+                this->data[i] = this->fp_raw[i];
+            }
+        }
+    }
+    fclose(fp);
+
+}
+
+void Wav_File::WriteWavFile(const char *dest_path) {
+    for (int i = 0; i < wav_size; i++) {
+        if (format_tag == 1)
+            this->ip_raw[i] = (int16_t) (this->data[i] * 32768);
+        else
+            this->fp_raw[i] = this->data[i];
+    }
+
+    FILE *fp = fopen(dest_path, "wb");
+    if (fp) {
+
+        fwrite(this->id_riff, sizeof(char), 4, fp);
+        fwrite(&this->file_size, sizeof(int16_t), 2, fp);
+        fwrite(this->id_wave, sizeof(char), 4, fp);
+        fwrite(this->id_fmt, sizeof(char), 4, fp);
+        fwrite(&this->format_length, sizeof(int16_t), 2, fp);
+        fwrite(&this->format_tag, sizeof(int16_t), 1, fp);
+        fwrite(&this->channels, sizeof(int16_t), 1, fp);
+        fwrite(&this->sample_rate, sizeof(int16_t), 2, fp);
+        fwrite(&this->avg_bytes_sec, sizeof(int16_t), 2, fp);
+        fwrite(&this->block_align, sizeof(int16_t), 1, fp);
+        fwrite(&this->bits_per_sample, sizeof(int16_t), 1, fp);
+        if (this->format_tag == 3) {
+            fwrite(&this->cb_size, sizeof(int16_t), 1, fp);
+        }
+        fwrite(this->id_data, sizeof(char), 4, fp);
+        fwrite(&this->data_size, sizeof(int16_t), 2, fp);
+        if (this->format_tag == 1) {
+            fwrite(this->ip_raw, sizeof(int16_t), this->wav_size, fp);
+        } else if (this->format_tag == 3) {
+            fwrite(this->fp_raw, sizeof(float), this->wav_size, fp);
+        }
+
+    }
+    fclose(fp);
+
+}
+
+void Wav_File::LoadPcmFile(const char *file_path) {
+    FILE *fp = fopen(file_path, "rb");
+
+    // 计算文件长度
+    fseek(fp, 0L, SEEK_END);
+    this->wav_size = ftell(fp) / sizeof(float);
+    fseek(fp, 0L, SEEK_SET);
+
+    //申请内存空间
+    this->fp_raw = (float *) malloc(this->wav_size * sizeof(float));
+    fread(this->fp_raw, sizeof(float), this->wav_size, fp);
+    fclose(fp);
+}
+
+void Wav_File::WritePcmFile(const char *dest_path) {
+    FILE *fp = fopen(dest_path, "wb");
+    fwrite(this->fp_raw, sizeof(float), this->wav_size, fp);
+    fclose(fp);
+}
+
+void Wav_File::setSTFT(int16_t frame_size, int16_t frame_shift, const char *win_type) {
+    this->frame_size = frame_size;
+    this->frame_shift = frame_shift;
+    this->fft_size = this->frame_size;
+    this->frame_num = this->wav_size / frame_shift + 1;
+
+    this->win_type = win_type;
+    this->setWindow(this->win_type);
+
+    spec = (Complex **) malloc((this->fft_size / 2 + 1) * sizeof(*spec));
+    for (int i = 0; i < (this->fft_size / 2 + 1); i++)
+        spec[i] = (Complex *) malloc(frame_num * sizeof(Complex));
+
+    mag = (float_t **) malloc((this->fft_size / 2 + 1) * sizeof(*mag));
+    for (int i = 0; i < (this->fft_size / 2 + 1); i++)
+        mag[i] = (float_t *) malloc(frame_num * sizeof(float_t));
+
+    phase = (float_t **) malloc((this->fft_size / 2 + 1) * sizeof(*phase));
+    for (int i = 0; i < (this->fft_size / 2 + 1); i++)
+        phase[i] = (float_t *) malloc(frame_num * sizeof(float_t));
+}
+
+void Wav_File::setWindow(const char *winType) {
+    double_t a0, a1;
+    if (strcmp(winType, "hamming") == 0) {
+        a0 = 0.540000000;
+        a1 = 1 - a0;
+    } else if (strcmp(winType, "hanning") == 0) {
+        a0 = 0.50000000;
+        a1 = 1 - a0;
+    }
+    int frac = this->frame_size - 1;
+    this->window = (float_t *) malloc(this->frame_size * sizeof(float_t));
+    for (int i = 0; i < this->frame_size; i++) {
+        this->window[i] = float(a0 - a1 * cos(PI * 2 * i / frac));
+    }
+}
+
+void Wav_File::STFT() {
+    std::vector<float_t> padding_data;
+    for (int i = 0; i < this->frame_shift; i++)
+        padding_data.push_back(this->data[this->frame_shift - i]);
+    for (int i = 0; i < this->wav_size; i++)
+        padding_data.push_back(this->data[i]);
+    for (int i = 0; i < this->frame_shift; i++)
+        padding_data.push_back(this->data[this->wav_size - i - 2]);
+
+    this->real = (float_t *) malloc(sizeof(float_t) * this->frame_size);
+    this->imag = (float_t *) malloc(sizeof(float_t) * this->frame_size);
+
+    for (int i = 0; i < this->frame_num; i++) {
+        for (int j = 0; j < this->frame_size; j++) {
+            this->real[j] = padding_data[i * this->frame_shift + j] * this->window[j];
+            this->imag[j] = 0.0;
+        }
+
+        FFT(this->real, this->imag, this->fft_size);
+
+        for (int j = 0; j <= this->fft_size / 2; j++) {
+            this->spec[j][i].real = this->real[j];
+            this->spec[j][i].imag = this->imag[j];
+        }
+    }
+}
+
+void Wav_File::ISTFT() {
+    this->real = (float_t *) malloc(sizeof(float_t) * this->frame_size);
+    this->imag = (float_t *) malloc(sizeof(float_t) * this->frame_size);
+    auto *temp = (float_t *) malloc(sizeof(float_t) * (data_size + 2 * frame_shift));
+    auto *ola = (float_t *) malloc(sizeof(float_t) * (data_size + 2 * frame_shift));
+
+    for (int i = 0; i < this->frame_num; i++) {
+        //��N_FFT/2+1����N_FFT(����Գ�)
+        for (int j = 0; j < this->fft_size / 2 + 1; j++) {
+            real[j] = spec[j][i].real;
+            imag[j] = spec[j][i].imag;
+        }
+        for (int j = this->fft_size / 2 + 1; j < this->fft_size; j++) {
+            real[j] = spec[this->fft_size - j][i].real;
+            imag[j] = -spec[this->fft_size - j][i].imag;
+        }
+
+        IFFT(real, imag, this->fft_size);
+
+        for (int j = 0; j < frame_size; j++) {
+            temp[i * frame_shift + j] += (real[j] * this->window[j]);
+            ola[i * frame_shift + j] += (this->window[j] * this->window[j]);
+        }
+    }
+
+    for (int i = 0; i < (frame_num + 1) * frame_shift; i++) {
+        if (temp[i] != 0) {
+            temp[i] /= ola[i];
+        }
+    }
+    for (int i = 0; i < (frame_num - 1) * frame_shift; i++)
+        this->data[i] = temp[i + frame_shift];
+
+//    超出部分就保持原样,不降噪就好了
+//    int sub = this->wav_size - (frame_num - 1) * frame_shift;
+//    this->file_size -= sub;
+//    this->wav_size -= sub;
+//    this->data_size -= sub;
+}
+
+void Wav_File::FFT(float_t param_real[], float_t param_imag[], int16_t param_n) {
+    float_t treal, timag, ureal, uimag, arg;
+    int32_t m, k, j, t, index1, index2;
+    auto *wreal = (float_t *) malloc(frame_shift * sizeof(float_t));
+    auto *wimag = (float_t *) malloc(frame_shift * sizeof(float_t));
+
+
+    this->bitrp(param_real, param_imag, param_n);
+
+    arg = -2 * PI / param_n;
+    treal = cosf(arg);
+    timag = sinf(arg);
+    wreal[0] = 1.0;
+    wimag[0] = 0.0;
+    for (j = 1; j < param_n / 2; j++) {
+        wreal[j] = wreal[j - 1] * treal - wimag[j - 1] * timag;
+        wimag[j] = wreal[j - 1] * timag + wimag[j - 1] * treal;
+    }
+
+    for (m = 2; m <= param_n; m *= 2) {
+        for (k = 0; k < param_n; k += m) {
+            for (j = 0; j < m / 2; j++) {
+                index1 = k + j;
+                index2 = index1 + m / 2;
+                t = param_n * j / m;
+                treal = wreal[t] * param_real[index2] - wimag[t] * param_imag[index2];
+                timag = wreal[t] * param_imag[index2] + wimag[t] * param_real[index2];
+                ureal = param_real[index1];
+                uimag = param_imag[index1];
+                param_real[index1] = ureal + treal;
+                param_imag[index1] = uimag + timag;
+                param_real[index2] = ureal - treal;
+                param_imag[index2] = uimag - timag;
+            }
+        }
+    }
+    free(wreal);
+    free(wimag);
+}
+
+void Wav_File::IFFT(float_t param_real[], float_t param_imag[], int16_t param_n) {
+    float_t treal, timag, ureal, uimag, arg;
+    int m, k, j, t, index1, index2;
+    auto *wreal = (float_t *) malloc(frame_shift * sizeof(float_t));
+    auto *wimag = (float_t *) malloc(frame_shift * sizeof(float_t));
+
+    bitrp(param_real, imag, param_n);
+
+    arg = 2 * PI / param_n;
+    treal = cosf(arg);
+    timag = sinf(arg);
+    wreal[0] = 1.0;
+    wimag[0] = 0.0;
+    for (j = 1; j < param_n / 2; j++) {
+        wreal[j] = wreal[j - 1] * treal - wimag[j - 1] * timag;
+        wimag[j] = wreal[j - 1] * timag + wimag[j - 1] * treal;
+    }
+
+    for (m = 2; m <= param_n; m *= 2) {
+        for (k = 0; k < param_n; k += m) {
+            for (j = 0; j < m / 2; j++) {
+                index1 = k + j;
+                index2 = index1 + m / 2;
+                t = param_n * j / m;
+                treal = wreal[t] * param_real[index2] - wimag[t] * param_imag[index2];
+                timag = wreal[t] * param_imag[index2] + wimag[t] * param_real[index2];
+                ureal = param_real[index1];
+                uimag = param_imag[index1];
+                param_real[index1] = ureal + treal;
+                param_imag[index1] = uimag + timag;
+                param_real[index2] = ureal - treal;
+                param_imag[index2] = uimag - timag;
+            }
+        }
+    }
+
+    for (j = 0; j < param_n; j++) {
+        real[j] /= param_n;
+        imag[j] /= param_n;
+    }
+    free(wreal);
+    free(wimag);
+}
+
+void Wav_File::bitrp(float_t param_real[], float_t param_imag[], int16_t param_n) {
+    int i, j, a, b, p;
+
+    for (i = 1, p = 0; i < param_n; i *= 2) {
+        p++;
+    }
+    for (i = 0; i < param_n; i++) {
+        a = i;
+        b = 0;
+        for (j = 0; j < p; j++) {
+            b = (b << 1) + (a & 1);    // b = b * 2 + a % 2;
+            a >>= 1;        // a = a / 2;
+        }
+        if (b > i) {
+            std::swap(param_real[i], param_real[b]);
+            std::swap(param_imag[i], param_imag[b]);
+        }
+    }
+}
+
+void Wav_File::FreeSource() {
+    free(this->ip_raw);
+    free(this->fp_raw);
+    free(this->real);
+    free(this->imag);
+    free(this->spec);
+    free(this->mag);
+    free(this->phase);
+}
+
+void Wav_File::getMagnitude() {
+    for (int i = 0; i <= this->fft_size / 2; i++)
+        for (int j = 0; j < frame_num; j++)
+            mag[i][j] = sqrtf(powf(spec[i][j].real, 2) + powf(spec[i][j].imag, 2));
+}
+
+void Wav_File::getPhase() {
+    for (int i = 0; i <= this->fft_size / 2; i++) {
+        for (int j = 0; j < frame_num; j++) {
+            phase[i][j] = atan2f(spec[i][j].imag, spec[i][j].real);
+        }
+    }
+}
+
+void Wav_File::magToSpec() {
+    for (int i = 0; i <= this->fft_size / 2; i++) {
+        for (int j = 0; j < frame_num; j++) {
+            spec[i][j].real = this->mag[i][j] * cosf(this->phase[i][j]);
+            spec[i][j].imag = this->mag[i][j] * sinf(this->phase[i][j]);
+        }
+    }
+}
+
+
+
+
+
