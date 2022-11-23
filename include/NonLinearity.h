612bc95923b7b236236c8d2e2eb1aec8c7c5901f
zhangkanghao <koerzhang@163.com>
2022-11-23 17:07:46 +0800

    NonLinearity ELU,Softplus

--- include/NonLinearity.h
+++ include/NonLinearity.h
@@ -12,32 +12,11 @@ class NonLinearity {
 public:
     NonLinearity();
 
-    NonLinearity(std::string type, bool inplace = false);
+    Eigen::Tensor<float_t, 4> ELU(Eigen::Tensor<float_t, 4> &input, float_t alpha = 1.0);
 
-    NonLinearity(std::string type, int64_t val, bool inplace = false);
+    Eigen::Tensor<float_t, 4> ReLU(Eigen::Tensor<float_t, 4> &input);
 
-    NonLinearity(std::string type, float_t val, bool inplace = false);
-
-    NonLinearity(std::string type, float_t beta, float_t threshold);
-
-    void ELU(Eigen::Tensor<float_t, 4> &input, Eigen::Tensor<float_t, 4> &output);
-
-    void ReLU(Eigen::Tensor<float_t, 4> &input, Eigen::Tensor<float_t, 4> &output);
-
-    void PReLU(Eigen::Tensor<float_t, 4> &input, Eigen::Tensor<float_t, 4> &output);
-
-    void Softplus(Eigen::Tensor<float_t, 4> &input, Eigen::Tensor<float_t, 4> &output);
-
-    void Sigmoid(Eigen::Tensor<float_t, 4> &input, Eigen::Tensor<float_t, 4> &output);
-
-
-private:
-    std::string type;
-    float_t alpha;
-    int64_t channels;
-    float_t beta;
-    float_t threshold;
-    bool inplace;
+    Eigen::Tensor<float_t, 4> Softplus(Eigen::Tensor<float_t, 4> &input, float_t beta = 1.0, float_t threshold = 20.0);
 
 };
 
